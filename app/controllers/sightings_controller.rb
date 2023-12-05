33e45a83e86d03f01eb20c998e1a86bc77a15094
kevinrojas <k3vinr07@gmail.com>
2023-12-05 15:18:57 -0800

    story 2 complete

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,46 @@
+class SightingsController < ApplicationController
+
+    def index
+        sightings = Sighting.all
+        render json: sightings
+    end
+
+    def show
+        sighting = Sighting.find(params[:id])
+        render json: sighting
+    end
+
+    def create
+        sighting = Sighting.find(sighting_params)
+        if sighting.valid?
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+    end
+
+    def update 
+        sighting = Sighting.find(params[:id])
+        sighting.update(sighting_params)
+        if sighting.valid?
+            render json: sighting
+        else
+            render json sighting.errors
+        end
+    end
+    
+    def destroy
+        sighting = Sighting.find(params[:id])
+        if sighting.destroy
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+    end
+
+    private
+    def sighting_params
+        params.require(:sighting).permit(:latitude, :longitude, :date, :animal_key)
+    end
+
+end
